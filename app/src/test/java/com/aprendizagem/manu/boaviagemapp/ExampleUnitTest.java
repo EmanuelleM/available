355b694ba6f2b9208445e013f06967afccb9e765
cs-emanuelle-menali <emanuelle.menali@concrete.com.br>
2017-09-18 11:19:19 -0300

    alteracao do arquivo de teste

--- app/src/test/java/com/aprendizagem/manu/boaviagemapp/ExampleUnitTest.java
+++ app/src/test/java/com/aprendizagem/manu/boaviagemapp/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package com.aprendizagem.manu.boaviagemapp;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.*;
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
+    @org.testng.annotations.Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
